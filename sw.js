fef41bc77359db1d3080a781c7aea43f6aacc916
MRGollapudi <venu.bezawada@gmail.com>
2025-06-01 18:02:50 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,19 +4,18 @@ const urlsToCache = [
   "./index.html",
   "./styles.css",
   "./script.js",
+  "./manifest.json",
   "./king-of-diamonds.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
-});
+}); 
